31b103d1ec5090172c61479aa98d35146da20e09
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-04-30 15:42:49 -0500

    seeding and model factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,12 +13,24 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
+  static $password;
 
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
-    ];
+  return [
+    'name' => $faker->name,
+    'email' => $faker->unique()->safeEmail,
+    'password' => $password ?: $password = bcrypt('secret'),
+    'remember_token' => str_random(10),
+  ];
+});
+/**
+* factory Messages
+*
+*/
+$factory->define(App\Message::class, function (Faker\Generator $faker){
+
+  return [
+    'content' => $faker->realText(random_int(20, 160)),
+    'image' => $faker->imageUrl(600,338),
+  ];
+
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,17 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+  /**
+  * Run the database seeds.
+  *
+  * @return void
+  */
+  public function run()
+  {
+    // $this->call(UsersTableSeeder::class);
+    // factory(App\Message::class)->create();
+    factory(App\Message::class)
+    ->times(100)
+    ->create();
+  }
 }
