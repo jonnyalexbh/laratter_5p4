5942ea4d65ec8a5fd8d70a2ce7d0292006ab1ab7
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-05-17 20:21:25 -0500

    authorization to send messages

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,29 +2,35 @@
 
 namespace App\Providers;
 
+use App\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
-    ];
+  /**
+  * The policy mappings for the application.
+  *
+  * @var array
+  */
+  protected $policies = [
+    'App\Model' => 'App\Policies\ModelPolicy',
+  ];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerPolicies();
+  /**
+  * Register any authentication / authorization services.
+  *
+  * @return void
+  */
+  public function boot()
+  {
+    $this->registerPolicies();
+    //
+    Gate::define('dms', function(User $user, User $other){
+      return
+      $user->isFollowing($other) &&
+      $other->isFollowing($user);
+    });
 
-        //
-    }
+  }
 }
