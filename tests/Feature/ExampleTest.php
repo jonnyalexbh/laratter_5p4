1a64ef2ff93613f87cf4732cfacf09582075c0ca
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-05-29 10:54:54 -0500

    unit testing in Laravel

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -9,15 +9,28 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+  /**
+  * A basic test example.
+  *
+  * @return void
+  */
+  public function testBasicTest()
+  {
+    // Arrange | Preparación
+    // Act | Acción
+    // Assert | Verificación
+    $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
+    $response->assertStatus(200);
+    $response->assertSee('Laratter');
+  }
+  /**
+  * testCanSearchForMessages
+  *
+  */
+  public function testCanSearchForMessages()
+  {
+    $response = $this->get('/messages?query=Alice');
+    $response->assertSee('Alice');
+  }
 }
---
+++ tests/Feature/UsersTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
+
+class UsersTest extends TestCase
+{
+
+  use DatabaseTransactions;
+  use InteractsWithDatabase;
+
+  /**
+  * testCanSeeUserPage
+  *
+  */
+  public function testCanSeeUserPage()
+  {
+    $user = factory(User::class)->create();
+
+    $response = $this->get($user->username);
+    $response->assertSee($user->name);
+  }
+  /**
+  * testCanLogin
+  *
+  */
+  public function testCanLogin()
+  {
+    $user = factory(User::class)->create();
+
+    $response = $this->post('/login', [
+      'email' => $user->email,
+      'password' => 'secret',
+    ]);
+
+    $this->seeIsAuthenticatedAs($user);
+  }
+  /**
+  * testCanFollow
+  *
+  */
+  public function testCanFollow()
+  {
+    $user = factory(User::class)->create();
+    $other = factory(User::class)->create();
+
+    $response = $this->actingAs($user)->post($other->username.'/follow');
+
+    $this->assertDatabaseHas('followers', [
+      'user_id' => $user->id,
+      'followed_id' => $other->id,
+    ]);
+  }
+}
