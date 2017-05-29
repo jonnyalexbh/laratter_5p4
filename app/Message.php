9c789100060be783d0a8b297be43bcb37030d569
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-05-29 10:12:24 -0500

    search for message author using algolia

--- app/Message.php
+++ app/Message.php
@@ -11,11 +11,17 @@ class Message extends Model
   use Searchable;
 
   protected $guarded = [];
-
+  /**
+  * user
+  *
+  */
   public function user(){
     return $this->belongsTo(User::class);
   }
-
+  /**
+  * getImageAttribute
+  *
+  */
   public function getImageAttribute($image)
   {
     if (!$image || starts_with($image, 'http')) {
@@ -23,6 +29,16 @@ class Message extends Model
     }
 
     return \Storage::disk('public')->url($image);
+  }
+  /**
+  * toSearchableArray
+  *
+  */
+  public function toSearchableArray()
+  {
+    $this->load('user');
+
+    return $this->toArray();
   }
 
 }
